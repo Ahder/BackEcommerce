c2d3d79f7af033ec4e890d2ef9abaaad6c3a099f
Redha <benzinered@yahoo.fr>
2019-04-05 14:08:23 +0200

    Init Project folder

---
+++ BackTechTools/src/main/java/com/fges/rizomm/tech/BackTechTools/BackTechToolsApplication.java
@@ -0,0 +1,13 @@
+package com.fges.rizomm.tech.BackTechTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTechToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTechToolsApplication.class, args);
+	}
+
+}
